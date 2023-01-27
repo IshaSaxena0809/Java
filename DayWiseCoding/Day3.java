e64e9163d76412feed0ab16910fd1d28f9aa3ad3
Isha saxena <96019320+IshaSaxena0809@users.noreply.github.com>
2023-01-28 02:43:01 +0530

    Add files via upload

---
+++ DayWiseCoding/Day3.java
@@ -0,0 +1,11 @@
+package DayWiseCoding;
+
+//Write a program to find ASCII values of a character
+public class Day3
+{
+    public static void main(String[] args) throws Exception{
+        int ch = System.in.read();
+        System.out.println(ch);
+    }
+
+}
