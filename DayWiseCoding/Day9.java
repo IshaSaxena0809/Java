8f88f8b274c8153200de506ad453f7912739acaf
Isha saxena <96019320+IshaSaxena0809@users.noreply.github.com>
2023-01-28 12:15:45 +0530

    Update Day9.java

--- DayWiseCoding/Day9.java
+++ DayWiseCoding/Day9.java
@@ -1,5 +1,5 @@
 package DayWiseCoding;
-
+//Take an integer as the input from the user and then calculate the number of digits in the given input and print it as the output.
 import java.util.Scanner;
 
 public class Day9 {
