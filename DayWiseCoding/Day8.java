1bb9c50cfe633f36cbdd5d289268ea9e5e08df7e
Isha saxena <96019320+IshaSaxena0809@users.noreply.github.com>
2023-01-28 12:15:04 +0530

    Update Day8.java

--- DayWiseCoding/Day8.java
+++ DayWiseCoding/Day8.java
@@ -1,5 +1,5 @@
 package DayWiseCoding;
-
+//Get the values of a, b and c (coefficients of quadratic equation) as input from the user and calculate the roots and print as the output.
 import java.util.Scanner;
 
 public class Day8 {
